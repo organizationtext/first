6956dcfed7bba43121ac191ccc657fe2536d4506
xiaobudian42027 <zhangguoqingxie@163.com>
2017-01-23 18:16:02 +0800

    001

---
+++ first/first/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  first
+//
+//  Created by xiaorenwu on 2017/1/23.
+//  Copyright © 2017年 xiaorenwu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ first/first/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  first
+//
+//  Created by xiaorenwu on 2017/1/23.
+//  Copyright © 2017年 xiaorenwu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
